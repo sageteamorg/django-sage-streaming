d7af908c3031f2f5fc4a1afae235a7a72ea937f1
Mehran Rahmanzadeh <mrhnz13@gmail.com>
2021-07-15 22:07:07 +0430

    feat (sage_stream--models): Create Log model
    - WatchLog model
    - logs each watch video request

---
+++ sage_stream/models.py
@@ -0,0 +1,43 @@
+from django.conf import settings
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+class WatchLog(models.Model):
+    """Stream watch log"""
+    video_path = models.CharField(
+        _('Video Path'),
+        max_length=255
+    )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_('User'),
+        related_name='watch_logs',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    is_authenticated = models.BooleanField(
+        _('Is Authenticated'),
+        default=False
+    )
+
+    ip = models.GenericIPAddressField(
+        _('IP Address'),
+        null=True,
+        blank=True
+    )
+
+    created = models.DateTimeField(
+        _('Created At'),
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return f'Watch Log ip: {self.ip}, created at {self.created}'
+
+    class Meta:
+        verbose_name = _('Watch Log')
+        verbose_name_plural = _('Watch Logs')
+        ordering = ('-created',)
